ecbc26c8453bdd4485a0d2913cd5b2c6a4ae7fd7
AlexandrNovik <wasjatka@gmial.com>
2023-01-29 18:29:15 +0300

    add solution

--- src/main/kotlin/task/MultiplyStrings.kt
+++ src/main/kotlin/task/MultiplyStrings.kt
@@ -3,13 +3,39 @@ package task
 // https://leetcode.com/problems/multiply-strings/description/
 fun main() {
     val s = MultiplyStrings()
-    println(s.multiply("28", "341"))
+    println(s.multiply("140", "721"))
 }
 
 class MultiplyStrings {
     fun multiply(num1: String, num2: String): String {
-        val result = takeMultiplyResultList(num1, num2)
-        return result.joinToString()
+        if (num1 == "0" || num2 == "0") return "0"
+        val resultReversed = takeMultiplyResultList(num1, num2)
+        val resultWithNulls = mutableListOf<String>()
+        resultReversed.forEachIndexed { index, s ->
+            resultWithNulls.add("0".repeat(index) + s)
+        }
+        val solutionList = mutableListOf<String>()
+        var index = 0
+        var transValue = 0
+        val maxCount = resultWithNulls.fold(0) { acc, s -> acc.coerceAtLeast(s.count()) }
+        while (true) {
+            if (index >= maxCount) {
+                if (transValue > 0){
+                    solutionList.add("1")
+                }
+                break
+            }
+            var result = 0
+            resultWithNulls.forEach {
+                result += it.getOrNull(index)?.toString()?.toInt() ?: 0
+            }
+            val intermediate = "${(result + transValue) % 10}"
+            transValue = (result + transValue) / 10
+            solutionList.add(intermediate)
+            index++
+        }
+
+        return solutionList.reversed().joinToString("")
     }
 
     private fun takeMultiplyResultList(num1: String, num2: String): List<String> {
@@ -17,19 +43,21 @@ class MultiplyStrings {
         val numReversed2 = num2.reversed()
 
         val multiplyList = mutableListOf<String>()
-        var transValue: Int
+        var transValue = 0
+        var intermediate = ""
         for (n2 in numReversed2) {
             transValue = 0
-            var intermediate = ""
+            intermediate = ""
             for (n1 in numReversed1) {
-                val result = n1.digitToInt() * n2.digitToInt()
-                intermediate = "${(result % 10) + transValue}$intermediate"
-                transValue = result / 10
+                val result = n1.toString().toInt() * n2.toString().toInt()
+                intermediate = "${((result % 10) + transValue) % 10}$intermediate"
+                transValue = (result + transValue) / 10
             }
             if (transValue > 0) {
                 intermediate = "$transValue$intermediate"
             }
-            multiplyList.add(intermediate)
+            println(intermediate)
+            multiplyList.add(intermediate.reversed())
         }
 
         return multiplyList
